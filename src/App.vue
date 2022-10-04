ec76892e288ada8b789c7f24efc7494bb76c00e9
albertgc1 <beto.17cv@gmail.com>
2022-10-04 11:44:20 -0400

    base project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+
+    <div>
+      <h2>Hello world!</h2>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
